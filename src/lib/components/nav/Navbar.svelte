d77324a997670f03b09187bacd60a028b576989a
Jack Foley <jackfdevelopment@gmail.com>
2025-12-20 10:20:44 +0000

    refactor navigation components for improved structure and consistency

--- src/lib/components/nav/Navbar.svelte
+++ src/lib/components/nav/Navbar.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	// css
 	import '@fortawesome/fontawesome-free/css/all.css';
-	import type { NavItem } from '$lib/components/nav/navbar'; // interface import
-	import NavbarSection from '$lib/components/nav/NavbarSection.svelte';
-	let { navItems }: { navItems: NavItem[][] } = $props();
+	
+	// ts
+	import { navItems } from './navbar';
+	
+	// components
+	import NavbarSection from './NavbarSection.svelte';
 </script>
 
 <div class="flex h-20 w-full justify-between border-b-2 border-b-[#232f44] bg-[#0b0d14] px-4">
