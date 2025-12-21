c544f70af99178e0ac2a7ccd26a5f00eece41ae7
Jack <jackfdevelopment@gmail.com>
2025-12-21 22:57:21 +0000

    pain and miseyr

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { ThemeButton } from '@/components/ThemeButton';
-import { useState } from 'react';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger
+} from "@/components/ui/navigation-menu"
 
 function App() {
-  const [theme, setTheme] = useState('light');
-
-  const handleThemeClick = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
 
   return (
-    <div className='overflow-hidden h-svh w-full relative'>
-      <ThemeButton theme={theme} onThemeChange={handleThemeClick} />
+    <div className='overflow-hidden h-svh w-full relative bg-[#0a0a0a]'>
+      <NavigationMenu >
+        <NavigationMenuList>
+          <NavigationMenuItem className="bg-#0a0a0a">
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 }
